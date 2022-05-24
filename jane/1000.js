f01bec81200cc8c21ecd78698ad88b2bb4723cf0
danrleypereira <danrleywillian@gmail.com>
2022-05-23 21:27:41 -0300

    adding hoisting and arrow function examples

---
+++ jane/1000.js
@@ -0,0 +1,25 @@
+/*
+Author: Janielle
+# Input
+This problem has no input.
+
+# Output
+You must print the message Hello World! and then the endline as shown below.
+*/
+// print2(hello);
+
+function print2(string) {
+    hello = "Hello World!"
+    console.log(string);
+}
+const print = (string) => {
+    console.log(string);
+}
+
+// print(hello);
+var hello = "Hello World!"
+print2(hello);
+
+
+
+
